d3ac5cdd5974ecddb52d6783ed71eabde91df33a
Sparticus403 <twil8980@740s-pl-332.local>
2016-09-14 09:56:39 -0600

    added text to monster project

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -15,5 +15,13 @@ public class MonsterController
 	public void start()
 	{
 		System.out.println("here is my monster: " + monster);
+		System.out.println("My monster has this many eyes: " + monster.getEyeCount());
+		if(monster.getEyeCount() > 3)
+		{
+			System.out.println("Thats a lot of eyes!! - Scary");
+		}
+		System.out.println("My monster has this many antennas: " + monster.getAntennaCount());
+		System.out.println("My monster has this many noses: " + monster.getNoseCount());
+		System.out.println("My monster has " + monster.)
 	}
 }
