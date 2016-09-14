855463945797df4972b0e62fc104edf557abb297
Sparticus403 <twil8980@740s-pl-332.local>
2016-09-14 09:19:20 -0600

    added start method

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,6 +1,29 @@
 package monster.controller;
 
+import monster.model.MarshmallowMonster;
+
 public class MonsterController
 {
+	private MarshmallowMonster monster;
+	
+	public MonsterController()
+	{
+		monster = new MarshmallowMonster("Fredrick",
+											3,
+											2,
+											false,
+											6,
+											1);
+	}
+	/* name is fredrick
+	 * arm = 6
+	 * antenna = 3
+	 * eyes = 2
+	 * no bellybutton
+	 * nose = 0 */
 
+	public void start()
+	{
+		System.out.println("here is my monster: " + monster);
+	}
 }
